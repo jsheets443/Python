94fd16cf97ae4ceb755ac521ab6da0f879da4fd4
jsheets443 <jsheets443@gmail.com>
2022-07-05 10:20:15 -0400

    first commit

---
+++ Barista.py
@@ -0,0 +1,22 @@
+print("Welcome to the coffee shop")
+#input variable in following format nameofvariable = input()
+name = input("What is your name?\n")
+#To add to string add + variablename + unless at the beginning r end of sentence
+print("Hello " + name + " Thank you for coming in today.\n\n")
+
+menu = "Coffee, Green Tea, Chai, Cappuccino"
+#\n means new line
+print(name + ", what would you like from our menu today? Here is what we are serving.\n" + menu)
+
+order = input()
+
+price = 8
+
+quantity = input("How many drinks would you like?\n")
+
+total = price * int(quantity)
+
+print()
+
+print("THat sounds wonderful, " + name + ", we'll have that " + order + " ready for you in a just a few moments")
+

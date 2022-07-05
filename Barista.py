8736a8385b2e04f5a23b4a5de25c8edf6c3f8115
jsheets443 <jsheets443@gmail.com>
2022-07-05 13:20:02 -0400

    If else statement

--- Barista.py
+++ Barista.py
@@ -1,8 +1,17 @@
+from subprocess import BELOW_NORMAL_PRIORITY_CLASS
+
+
 print("Welcome to the coffee shop")
 #input variable in following format nameofvariable = input()
 name = input("What is your name?\n")
 #To add to string add + variablename + unless at the beginning r end of sentence
-print("Hello " + name + " Thank you for coming in today.\n\n")
+#if else statements
+#ctrl+tab backsteps off If for else
+if name == "Ben":
+    print("You're not welcome here evil Ben, get out!!!")
+    exit()
+else:
+    print("Hello " + name + " Thank you for coming in today.\n\n")
 
 menu = "Coffee, Green Tea, Chai, Cappuccino"
 #\n means new line
@@ -13,9 +22,8 @@ order = input()
 price = 8
 
 quantity = input("How many drinks would you like?\n")
-
+#convert input from one type of variable to another
 total = price * int(quantity)
 
-print("Thank you for your order, your total is  " + str(total))
-
-print("That sounds wonderful, " + name + ", we'll have that " + order + " ready for you in a just a few moments")
+print("Thank you for your order, your total is $" + str(total))
+print("That sounds wonderful, " + name + ", we'll have your " + quantity + " " + order + " ready for you in a just a few moments")
